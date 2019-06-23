f3f115188774ebace06b3175bcae89b5ee0c9659
Jeffrey <jeffreysantana26@gmail.com>
2019-06-22 23:58:22 -0500

    Added background image of Chicago River

--- client/src/app/components/coming-soon/coming-soon.component.ts
+++ client/src/app/components/coming-soon/coming-soon.component.ts
@@ -1,40 +1,45 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-coming-soon',
   templateUrl: './coming-soon.component.html',
-  styleUrls: ['./coming-soon.component.css']
+  styleUrls: ['./coming-soon.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ComingSoonComponent implements OnInit {
 
   constructor(@Inject(DOCUMENT) document) {
-    // Set the date we're counting down to
-    var countDownDate = new Date("Jul 1, 2019 00:00:00").getTime();
+    const launchDate = new Date('Jul 1, 2019 00:00:00').getTime();
 
-    // Update the count down every 1 second
-    var x = setInterval(function () {
+    //Update every second
+    const intvl = setInterval(() => {
+      //Get today's date
+      const now = new Date().getTime();
 
-      // Get todays date and time
-      var now = new Date().getTime();
+      //Distance from now to launchDate
+      const distance = launchDate - now;
 
-      // Find the distance between now an the count down date
-      var distance = countDownDate - now;
+      //Time calculations for hours, minutes, seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      //Display result
+      document.querySelector(".countdown").innerHTML = `
+        <div>${days}<span>Days</span></div>
+        <div>${hours}<span>Hours</span></div>
+        <div>${minutes}<span>Minutes</span></div>
+        <div>${seconds}<span>Seconds</span></div>
+      `;
 
-      // Display the result in an element with id="demo"
-      document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-      // If the count down is finished, write some text
+      // If launch date passed
       if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
+        clearInterval(intvl);
+
+        document.querySelector(".countdown").style.color = '#17a2b8';
+        document.querySelector(".countdown").innerHTML = 'Launched';
       }
     }, 1000);
   }
